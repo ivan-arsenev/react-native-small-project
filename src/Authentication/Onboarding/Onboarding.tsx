23ea566ff5b135f53fa272e76ca39e872b2ef430
Ivan Arsenev <iv.arsenev@gmail.com>
2021-01-27 17:54:35 +0300

    InitialSetup

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+
+import React from 'react'
+
+const Onboarding = () => {
+    return (
+        <View style={{ flex: 1, backgroundColor: 'cyan' }}>
+            <Text> textInComponent </Text>
+        </View>
+    )
+}
+
+export default Onboarding
